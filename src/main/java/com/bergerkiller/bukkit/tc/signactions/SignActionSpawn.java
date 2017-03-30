2b4f069791517bc389cbb4ce40d1eb5bee1e03d0
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2017-03-31 00:08:43 +0200

    Spawn sign: new functionality for spawning left/right/center

--- src/main/java/com/bergerkiller/bukkit/tc/signactions/SignActionSpawn.java
+++ src/main/java/com/bergerkiller/bukkit/tc/signactions/SignActionSpawn.java
@@ -3,6 +3,8 @@ package com.bergerkiller.bukkit.tc.signactions;
 import com.bergerkiller.bukkit.common.collections.BlockMap;
 import com.bergerkiller.bukkit.common.config.DataReader;
 import com.bergerkiller.bukkit.common.config.DataWriter;
+import com.bergerkiller.bukkit.common.utils.FaceUtil;
+import com.bergerkiller.bukkit.common.utils.LogicUtil;
 import com.bergerkiller.bukkit.common.utils.ParseUtil;
 import com.bergerkiller.bukkit.common.utils.StringUtil;
 import com.bergerkiller.bukkit.common.utils.WorldUtil;
@@ -27,7 +29,10 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
 
 public class SignActionSpawn extends SignAction {
@@ -42,6 +47,7 @@ public class SignActionSpawn extends SignAction {
         addSpawnType('h', EntityType.MINECART_HOPPER);
         addSpawnType('t', EntityType.MINECART_TNT);
         addSpawnType('e', EntityType.MINECART_MOB_SPAWNER);
+        addSpawnType('c', EntityType.MINECART_COMMAND);
     }
 
     static {
@@ -51,6 +57,7 @@ public class SignActionSpawn extends SignAction {
         addPermType('h', Permission.SPAWNER_HOPPER);
         addPermType('t', Permission.SPAWNER_TNT);
         addPermType('e', Permission.SPAWNER_SPAWNER);
+        addPermType('c', Permission.SPAWNER_COMMAND);
     }
 
 
@@ -120,6 +127,9 @@ public class SignActionSpawn extends SignAction {
         boolean validCartTypeSpecified = false; // If no valid cart types failed, fail
         int cartAmountSpecified = -1;           // Keep track of numbers, to avoid things like "0m"
         for (char cart : (event.getLine(2) + event.getLine(3)).toCharArray()) {
+            if (isLeftChar(cart) || isRightChar(cart)) {
+                continue;
+            }
             if (Character.isDigit(cart)) {
                 if (cartAmountSpecified == -1) {
                     // This is the first digit of the cart specification
@@ -158,96 +168,224 @@ public class SignActionSpawn extends SignAction {
             final double spawnForce = getSpawnForce(info);
 
             //Get the cart types to spawn
-            ArrayList<EntityType> types = new ArrayList<>();
-            StringBuilder amountBuilder = new StringBuilder();
-            EntityType type;
-            for (char cart : (info.getLine(2) + info.getLine(3)).toCharArray()) {
-                type = minecartTypes.get(Character.toString(cart));
-                if (type != null) {
-                    if (amountBuilder.length() > 0) {
-                        int amount = ParseUtil.parseInt(amountBuilder.toString(), 1);
-                        amountBuilder.setLength(0);
-                        for (int i = 0; i < amount; i++) {
-                            types.add(type);
-                        }
-                    } else {
-                        types.add(type);
-                    }
-                } else if (Character.isDigit(cart)) {
-                    amountBuilder.append(cart);
+            SpawnTypes types = getSpawnTypes(info.getLine(2) + info.getLine(3));
+            if (types.types.isEmpty()) {
+                return;
+            }
+
+            // Find minecart spawn position information for each possible direction
+            int sizeLim = ((types.types.size() - 1) / 2) + 1;
+            List<SpawnPositions> modes = new ArrayList<SpawnPositions>();
+            for (BlockFace direction : info.getWatchedDirections()) {
+                SpawnPositions mode = getSpawnPositions(info, direction.getOppositeFace(), types.types.size());
+                if (mode.locs.size() >= sizeLim) {
+                    modes.add(mode);
+                }
+            }
+            Collections.sort(modes);
+            if (modes.isEmpty()) {
+                return;
+            }
+
+            // Trim off modes > 2, we don't use those
+            while (modes.size() > 2) {
+                modes.remove(modes.size() - 1);
+            }
+
+            // Figure out what spawn direction we should launch the train into after spawn
+            BlockFace launchDirection = BlockFace.SELF;
+            for (SpawnPositions mode : modes) {
+                if (mode.powered) {
+                    launchDirection = mode.direction;
                 }
             }
 
-            if (types.isEmpty()) {
-                return;
-            }
-            Location[] locs = new Location[types.size()];
-            BlockFace spawnDirection = null;
-            if (types.size() == 1) {
-                // Single-minecart spawning logic
-                locs[0] = info.getCenterLocation();
-                if (MinecartMemberStore.getAt(locs[0]) == null) {
-                    for (BlockFace direction : info.getWatchedDirections()) {
-                        direction = direction.getOppositeFace();
-                        spawnDirection = direction;
-                        TrackIterator iter = new TrackIterator(info.getRails(), direction);
-                        // Ignore the starting block
-                        iter.next();
-                        // Next block available?
-                        if (iter.hasNext()) {
-                            break;
-                        }
+            // If the sign is powered from a non-direction and multiple ways are possible, figure a direction out
+            // If none can be figured out based on centering modes, resolve to spawning in the center without launching
+            if (launchDirection == BlockFace.SELF) {
+                if (modes.size() >= 2) {
+                    // Centering is possible; more than one direction can be spawned
+                    if (types.centerMode == CenterMode.LEFT) {
+                        launchDirection = FaceUtil.rotate(info.getFacing(), 2);
+                    } else if (types.centerMode == CenterMode.RIGHT) {
+                        launchDirection = FaceUtil.rotate(info.getFacing(), -2);
+                    } else {
+                        types.centerMode = CenterMode.MIDDLE;
+                        launchDirection = BlockFace.SELF;
                     }
+                    if (types.centerMode != CenterMode.MIDDLE) {
+                        int minAngle = 1000;
+                        SpawnPositions selectedMode = modes.get(0);
+                        for (SpawnPositions mode : modes) {
+                            int angle = FaceUtil.getFaceYawDifference(mode.direction, launchDirection);
+                            if (angle < minAngle) {
+                                minAngle = angle;
+                                selectedMode = mode;
+                            }
+                        }
+                        launchDirection = selectedMode.direction;
+                    }
+                } else {
+                    // Centering not possible. Restrict to one mode only.
+                    types.centerMode = CenterMode.NONE;
+                    launchDirection = modes.get(0).direction;
+                }
+            }
+
+            // Find desired spawn locations for the minecarts
+            List<Location> spawnLocations = new ArrayList<Location>(types.types.size());
+            if (types.centerMode == CenterMode.MIDDLE && modes.size() >= 2) {
+                // Center-Mode: combine two directions into one long stretch of minecarts
+                Iterator<Location> iter0 = modes.get(0).locs.iterator();
+                Iterator<Location> iter1 = modes.get(1).locs.iterator();
+
+                // Add and skip middle cart
+                spawnLocations.add(iter0.next());
+                iter1.next();
+
+                boolean mode = false; // alternates iter0/iter1
+                while (spawnLocations.size() < types.types.size()) {
+                    if (mode && iter0.hasNext()) {
+                        spawnLocations.add(iter0.next());
+                    } else if (iter1.hasNext()) {
+                        spawnLocations.add(0, iter1.next());
+                    } else {
+                        break; // failure
+                    }
+                    mode = !mode;
                 }
             } else {
-                // Multiple-minecart spawning logic
-                for (BlockFace direction : info.getWatchedDirections()) {
-                    direction = direction.getOppositeFace();
-                    TrackWalkIterator iter = new TrackWalkIterator(info.getCenterLocation(), direction);
-                    boolean occupied = false;
-                    for (int i = 0; i < types.size(); i++) {
-                        if (!iter.hasNext()) {
-                            occupied = true;
-                            break;
+                // Spawn direction from center mode. Default to the launch direction.
+                BlockFace spawnDirection = launchDirection;
+                if (types.centerMode == CenterMode.LEFT) {
+                    spawnDirection = FaceUtil.rotate(info.getFacing(), 2);
+                } else if (types.centerMode == CenterMode.RIGHT) {
+                    spawnDirection = FaceUtil.rotate(info.getFacing(), -2);
+                }
+
+                // Figure out the best direction to spawn in and take over those locations
+                // This defaults to the longest ([0]), and favors the direction in which we launch
+                SpawnPositions selectedMode = modes.get(0);
+                int minAngle = 1000;
+                for (SpawnPositions mode : modes) {
+                    if (mode.locs.size() < types.types.size()) {
+                        if (mode.direction == launchDirection) {
+                            launchDirection = BlockFace.SELF; // invalidate, cant launch there
                         }
-                        locs[i] = iter.next();
-                        //not taken?
-                        if (MinecartMemberStore.getAt(locs[i]) != null) {
-                            occupied = true;
-                            break;
-                        }
+                        continue;
                     }
-                    if (!occupied) {
-                        spawnDirection = direction;
-                        break;
+
+                    int angle = FaceUtil.getFaceYawDifference(mode.direction, spawnDirection);
+                    if (angle < minAngle && mode.locs.size() >= types.types.size()) {
+                        minAngle = angle;
+                        selectedMode = mode;
                     }
                 }
+                spawnLocations.addAll(selectedMode.locs);
+
+                // Invalidated launch direction? Use spawn direction instead.
+                if (launchDirection == BlockFace.SELF) {
+                    launchDirection = selectedMode.direction;
+                }
             }
-            // Check for failure
-            if (spawnDirection == null) {
-                return;
+            if (spawnLocations.size() < types.types.size()) {
+                return; // failed
             }
 
             // Prepare chunks
-            for (Location loc : locs) {
+            for (Location loc : spawnLocations) {
                 WorldUtil.loadChunks(loc, 2);
             }
 
             //Spawn
-            if (spawnDirection != null) {
-                MinecartGroup group = MinecartGroup.create();
-                for (int i = 0; i < locs.length; i++) {
-                    MinecartMember<?> mm = MinecartMemberStore.spawn(locs[i], types.get(i));
-                    group.add(mm);
-                    if (spawnForce != 0 && i == 0) {
-                        mm.getActions().addActionLaunch(spawnDirection, 2, spawnForce);
-                    }
+            MinecartGroup group = MinecartGroup.create();
+            for (int i = 0; i < spawnLocations.size(); i++) {
+                MinecartMember<?> mm = MinecartMemberStore.spawn(spawnLocations.get(i), types.types.get(i));
+                group.add(mm);
+                if (spawnForce != 0 && i == 0 && launchDirection != BlockFace.SELF) {
+                    mm.getActions().addActionLaunch(launchDirection, 2, spawnForce);
                 }
-                group.updateDirection();
-                group.getProperties().setDefault("spawner");
-                GroupCreateEvent.call(group);
+            }
+            group.updateDirection();
+            group.getProperties().setDefault("spawner");
+            GroupCreateEvent.call(group);
+        }
+    }
+
+    public static SpawnTypes getSpawnTypes(String text) {
+        SpawnTypes result = new SpawnTypes();
+        result.centerMode = CenterMode.NONE;
+        StringBuilder amountBuilder = new StringBuilder();
+        EntityType type;
+        for (char cart : text.toCharArray()) {
+            // Center-Mode designating characters
+            if (isLeftChar(cart)) {
+                result.addCenterMode(CenterMode.LEFT);
+                continue;
+            }
+            if (isRightChar(cart)) {
+                result.addCenterMode(CenterMode.RIGHT);
+                continue;
+            }
+
+            // Types and amounts for each type
+            type = minecartTypes.get(Character.toString(cart));
+            if (type != null) {
+                if (amountBuilder.length() > 0) {
+                    int amount = ParseUtil.parseInt(amountBuilder.toString(), 1);
+                    amountBuilder.setLength(0);
+                    for (int i = 0; i < amount; i++) {
+                        result.types.add(type);
+                    }
+                } else {
+                    result.types.add(type);
+                }
+            } else if (Character.isDigit(cart)) {
+                amountBuilder.append(cart);
             }
         }
+        return result;
+    }
+
+    /**
+     * Gets the Minecart spawn positions into a certain drection.
+     * The first location is always the middle on top of the current rail of the sign.
+     * 
+     * @param info sign event information
+     * @param direction of spawning
+     * @param nLimit limit amount of minecarts to spawn where we can stop looking for more spaces
+     * @return SpawnPositions with locs limited to the amount that could be spawned
+     */
+    public static SpawnPositions getSpawnPositions(SignActionEvent info, BlockFace direction, int nLimit) {
+        SpawnPositions result = new SpawnPositions();
+        result.direction = direction;
+        result.powered = info.isPowered(direction);
+        Location centerLoc = info.getCenterLocation();
+        if (nLimit == 1) {
+            // Single-minecart spawning logic
+            if (MinecartMemberStore.getAt(centerLoc) == null) {
+                TrackIterator iter = new TrackIterator(info.getRails(), direction);
+                // Ignore the starting block
+                iter.next();
+                // Next block available?
+                if (iter.hasNext()) {
+                    result.locs.add(centerLoc);
+                }
+            }
+        } else {
+            // Multiple-minecart spawning logic
+            TrackWalkIterator iter = new TrackWalkIterator(info.getCenterLocation(), direction);
+            for (int i = 0; i < nLimit && iter.hasNext(); i++) {
+                // Next location, not taken?
+                Location loc = iter.next();
+                if (MinecartMemberStore.getAt(loc) == null) {
+                    result.locs.add(loc);
+                } else {
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
     public static void init(String filename) {
@@ -312,4 +450,41 @@ public class SignActionSpawn extends SignAction {
     public void destroy(SignActionEvent info) {
         remove(info.getBlock());
     }
+
+    private static boolean isLeftChar(char c) {
+        return LogicUtil.containsChar(c, "]>)}");
+    }
+
+    private static boolean isRightChar(char c) {
+        return LogicUtil.containsChar(c, "[<({");
+    }
+
+    private static class SpawnTypes {
+        public CenterMode centerMode = CenterMode.MIDDLE;
+        public List<EntityType> types = new ArrayList<EntityType>();
+
+        public void addCenterMode(CenterMode mode) {
+            if (this.centerMode == CenterMode.NONE || this.centerMode == mode) {
+                this.centerMode = mode;
+            } else {
+                this.centerMode = CenterMode.MIDDLE;
+            }
+        }
+    }
+
+    private static class SpawnPositions implements Comparable<SpawnPositions> {
+        public BlockFace direction;
+        public List<Location> locs = new ArrayList<Location>();
+        public boolean powered;
+
+        @Override
+        public int compareTo(SpawnPositions o) {
+            return o.locs.size() - locs.size();
+        }
+    }
+
+    private static enum CenterMode {
+        NONE, MIDDLE, LEFT, RIGHT
+    }
+    
 }
