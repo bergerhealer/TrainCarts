4b9a6f5c8275b4e8a9927f70056ec38e2f5a7e1f
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2017-12-04 22:19:03 +0100

    Remove unused and broken trackmap functions

--- src/main/java/com/bergerkiller/bukkit/tc/utils/TrackMap.java
+++ src/main/java/com/bergerkiller/bukkit/tc/utils/TrackMap.java
@@ -1,11 +1,8 @@
 package com.bergerkiller.bukkit.tc.utils;
 
 import com.bergerkiller.bukkit.common.utils.BlockUtil;
-import com.bergerkiller.bukkit.common.utils.FaceUtil;
-import com.bergerkiller.bukkit.common.utils.MathUtil;
 import com.bergerkiller.bukkit.tc.rails.type.RailType;
 
-import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 
@@ -72,77 +69,6 @@ public class TrackMap extends ArrayList<Block> {
 
     public BlockFace getDirection() {
         return this.iterator.currentDirection();
-    }
-
-    public BlockFace getDirection(Block from) {
-        return getDirection(this.indexOf(from));
-    }
-
-    public BlockFace getDirection(int index) {
-        if (index > this.size() - 2) return this.getDirection();
-        if (index < 0) return BlockFace.SELF;
-        Location from = this.get(index).getLocation();
-        Location to = this.get(index + 1).getLocation();
-        int motX = to.getBlockX() - from.getBlockX();
-        int motZ = to.getBlockZ() - from.getBlockZ();
-        if (motX == 0 && motZ == 0) {
-            return BlockFace.SELF;
-        } else {
-            return FaceUtil.getDirection(motX, motZ, false);
-        }
-    }
-
-    public Location getPoint(int index) {
-        Location loc = this.get(index).getLocation();
-        BlockFace dir = getDirection(index);
-        loc.setYaw(FaceUtil.faceToYaw(dir) + 90);
-        return loc.add(0.5, 0, 0.5);
-    }
-
-    public Location[] getPoints() {
-        Location[] rval = new Location[this.size()];
-        for (int i = 0; i < rval.length; i++) {
-            rval[i] = getPoint(i);
-        }
-        return rval;
-    }
-
-    public Location[] walk(int stepCount, double stepSize) {
-        if (stepCount == 0) return new Location[0];
-        Location[] guide = getPoints();
-        Location[] rval = new Location[stepCount];
-        if (guide.length == 0) return new Location[0];
-        rval[0] = guide[0];
-        int guideindex = 1;
-        for (int i = 1; i < stepCount; i++) {
-            Location prev = rval[i - 1];
-            double towalk = stepSize;
-
-
-            if (guideindex > guide.length - 1) {
-                //End of the tracks, no movement possible
-                rval[i] = rval[i - 1];
-            } else {
-                //Subtract whole block distances (switch to next guideline)
-                while (true) {
-                    double tmpnext = Double.MAX_VALUE;
-                    if (guideindex < guide.length - 1) {
-                        tmpnext = prev.distance(guide[guideindex]);
-                    }
-                    if (tmpnext <= towalk) {
-                        towalk -= tmpnext;
-                        prev = guide[guideindex];
-                        guideindex++;
-                    } else {
-                        //Use a staged point to add the remaining distance
-                        prev = MathUtil.lerp(prev, guide[guideindex], towalk / tmpnext);
-                        break;
-                    }
-                }
-                rval[i] = prev;
-            }
-        }
-        return rval;
     }
 
     public TrackIterator getTrackIterator() {
