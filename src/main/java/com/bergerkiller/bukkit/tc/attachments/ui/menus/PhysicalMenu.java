038d875ca8e593aecc32d723de589916a6d4ab46
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2017-12-13 01:45:23 +0100

    Add start of physical menu

---
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/ui/menus/PhysicalMenu.java
@@ -0,0 +1,9 @@
+package com.bergerkiller.bukkit.tc.attachments.ui.menus;
+
+/**
+ * Menu for setting up the cart length, wheel distance, wheel centering,
+ * banking and movement drag parameters.
+ */
+public class PhysicalMenu {
+    //TODO!
+}
