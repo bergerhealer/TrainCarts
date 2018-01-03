120c73de3c52564907b282aeda9d623671e9df50
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2018-01-03 19:28:16 +0100

    Align attachment model correctly based on left/right hand

--- src/main/java/com/bergerkiller/bukkit/tc/attachments/control/CartAttachmentItem.java
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/control/CartAttachmentItem.java
@@ -5,6 +5,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
+import com.bergerkiller.bukkit.common.math.Matrix4x4;
+import com.bergerkiller.bukkit.common.math.Quaternion;
+import com.bergerkiller.bukkit.common.math.Vector3;
+import com.bergerkiller.bukkit.common.utils.DebugUtil;
 import com.bergerkiller.bukkit.common.utils.PacketUtil;
 import com.bergerkiller.bukkit.common.wrappers.DataWatcher;
 import com.bergerkiller.bukkit.tc.attachments.VirtualEntity;
@@ -35,6 +39,20 @@ public class CartAttachmentItem extends CartAttachment {
         this.entity.setHasRotation(true);
         this.entity.setRelativeOffset(0.0, -1.2, 0.0);
         this.entity.getMetaData().set(EntityHandle.DATA_FLAGS, (byte) EntityHandle.DATA_FLAG_INVISIBLE);
+
+        Vector3 poseTransform = new Vector3(0.0, 0.0, 0.0);
+        if (this.transformType == ItemTransformType.LEFT_HAND) {
+            poseTransform.x = -0.4;
+            poseTransform.y = 0.3;
+            poseTransform.z = 0.9375;
+        } else if (this.transformType == ItemTransformType.RIGHT_HAND) {
+            poseTransform.x = -0.4;
+            poseTransform.y = 0.3;
+            poseTransform.z = -0.9375;
+        }
+        this.local_transform = new Matrix4x4();
+        this.local_transform.translate(Vector3.add(this.position, poseTransform));
+        this.local_transform.rotateYawPitchRoll(this.rotation);
     }
 
     @Override
@@ -76,7 +94,15 @@ public class CartAttachmentItem extends CartAttachment {
 
         // Convert the pitch/roll into an appropriate pose
         Vector in_ypr = this.entity.getYawPitchRoll();
-        Vector rotation = new Vector(90.0, 90.0 + in_ypr.getZ(), 90.0 - in_ypr.getX());
+
+        Vector rotation;
+        if (this.transformType == ItemTransformType.LEFT_HAND) {
+            rotation = new Vector(180.0, -90.0 + in_ypr.getZ(), 90.0 - in_ypr.getX());
+        } else if (this.transformType == ItemTransformType.RIGHT_HAND) {
+            rotation = new Vector(0.0, 90.0 + in_ypr.getZ(), 90.0 - in_ypr.getX());
+        } else {
+            rotation = new Vector(90.0, 90.0 + in_ypr.getZ(), 90.0 - in_ypr.getX());
+        }
 
         DataWatcher meta = this.entity.getMetaData();
         if (this.transformType == ItemTransformType.HEAD) {
