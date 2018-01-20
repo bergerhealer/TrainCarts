691efa37bf94cca5b7f14146e574eea263ffacda
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2018-01-20 18:58:10 +0100

    Show text warning when not editing a cart in attachment editor

--- src/main/java/com/bergerkiller/bukkit/tc/attachments/ui/AttachmentEditor.java
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/ui/AttachmentEditor.java
@@ -4,9 +4,11 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
 
 import com.bergerkiller.bukkit.common.events.map.MapStatusEvent;
+import com.bergerkiller.bukkit.common.map.MapColorPalette;
 import com.bergerkiller.bukkit.common.map.MapDisplay;
 import com.bergerkiller.bukkit.common.map.MapSessionMode;
 import com.bergerkiller.bukkit.common.map.widgets.MapWidget;
+import com.bergerkiller.bukkit.common.map.widgets.MapWidgetText;
 import com.bergerkiller.bukkit.common.map.widgets.MapWidgetWindow;
 import com.bergerkiller.bukkit.tc.attachments.config.AttachmentModel;
 import com.bergerkiller.bukkit.tc.attachments.ui.MapWidgetAttachmentNode.MenuItem;
@@ -58,89 +60,27 @@ public class AttachmentEditor extends MapDisplay {
         this.setSessionMode(MapSessionMode.HOLDING);
         this.setMasterVolume(0.3f);
 
-        /*
-        CartProperties properties = CartProperties.getEditing(this.getViewers().get(0));
-        if (properties == null) {
-            this.member = null;
-            this.setRunning(false);
-            return;
-        } else {
-            this.member = properties.getHolder();
-        }
-        */
-
-        //getLayer().draw(this.loadTexture("com/bergerkiller/bukkit/tc/textures/attachments/background.png"), 0, 0);
-        //getLayer(1).draw(MapFont.MINECRAFT, 5, 5, MapColorPalette.COLOR_BLACK, "Minecart attachments");
-
         MapWidgetWindow window = new MapWidgetWindow();
         window.setBounds(0, 0, getWidth(), getHeight());
         window.getTitle().setText("Attachment Editor");
+        this.addWidget(window);
 
         CartProperties prop = CartProperties.getEditing(this.getOwners().get(0));
         if (prop != null) {
             this.model = prop.getModel();
+            this.tree.setModel(this.model);
+            window.addWidget(this.tree.setBounds(5, 13, 7 * 17, 6 * 17));
+
+            this.setReceiveInputWhenHolding(true);
         } else {
             this.model = AttachmentModel.getDefaultModel(EntityType.MINECART);
+
+            window.addWidget(new MapWidgetText())
+                .setText("Please select the\nMinecart to edit!")
+                .setColor(MapColorPalette.COLOR_RED)
+                .setShadowColor(MapColorPalette.getSpecular(MapColorPalette.COLOR_RED, 0.5f))
+                .setPosition(20, 60);
         }
-
-        this.tree.setModel(this.model);
-        window.addWidget(this.tree.setBounds(5, 13, 7 * 17, 6 * 17));
-        
-        
-        
-        /*
-        window.addWidget(new MapWidgetButton().setBounds(10, 30, 32, 32));
-        window.addWidget(new MapWidgetButton().setBounds(50, 30, 32, 32));
-        window.addWidget(new MapWidgetButton().setBounds(90, 30, 32, 32));
-        window.addWidget(new MapWidgetButton().setBounds(10, 70, 32, 32));
-        window.addWidget(new MapWidgetButton().setBounds(50, 70, 32, 32));
-        window.addWidget(new MapWidgetButton().setBounds(90, 70, 32, 32));
-        
-        window.addWidget(new MapWidgetButton().setBounds(50, 10, 32, 16));
-        window.addWidget(new MapWidgetButton().setBounds(50, 108, 32, 16));
-        
-        window.addWidget(new MapWidgetButton().setBounds(0, 50, 8, 8));
-        window.addWidget(new MapWidgetButton().setBounds(122, 50, 8, 8));
-        */
-        
-        this.addWidget(window);
-        
-        this.setReceiveInputWhenHolding(true);
-        
-        /*
-        this.addWidget(new MapWidget() {
-            int dx = 1;
-            int dy = 1;
-
-            @Override
-            public void onAttached() {
-                this.setBounds(10, 32, 32, 32);
-
-                this.addWidget(new MapWidget() {
-                    @Override
-                    public void onDraw() {
-                        this.view.fillItem(MapResourcePack.SERVER, new ItemStack(Material.DIAMOND));
-                    }
-                }.setBounds(8, 8, 16, 16));
-            }
-
-            @Override
-            public void onTick() {
-                this.setPosition(this.getX() + dx, this.getY() + dy);
-                if (this.getX() == 90 || this.getX() == 10) {
-                    this.dx = -this.dx;
-                }
-                if (this.getY() == 90 || this.getY() == 32) {
-                    this.dy = -this.dy;
-                }
-            }
-
-            @Override
-            public void onDraw() {
-                view.fillItem(MapResourcePack.SERVER, new ItemStack(Material.WORKBENCH));
-            }
-        });
-        */
     }
 
     public boolean acceptItem(ItemStack item) {
