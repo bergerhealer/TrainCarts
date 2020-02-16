339b7f0234410f430bb0f83d990c82d2c735ad96
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2020-02-16 17:34:52 +0100

    Disable seat attachment exit pos 20 tick preview interval
    Way too glitchy when the player does not have proper footing.
    Previewing when releasing is good enough.

--- src/main/java/com/bergerkiller/bukkit/tc/attachments/ui/menus/appearance/SeatMapWidgetNumberBox.java
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/ui/menus/appearance/SeatMapWidgetNumberBox.java
@@ -4,10 +4,8 @@ import com.bergerkiller.bukkit.common.map.MapEventPropagation;
 import com.bergerkiller.bukkit.tc.attachments.ui.MapWidgetNumberBox;
 
 public class SeatMapWidgetNumberBox extends MapWidgetNumberBox {
-    private static final int PREVIEW_RATE = 20;
     private final SeatExitPositionMenu menu;
     private final String field;
-    private int editCtr = PREVIEW_RATE;
     private boolean ignoreValueChange = true;
 
     public SeatMapWidgetNumberBox(SeatExitPositionMenu menu, String field) {
@@ -32,11 +30,6 @@ public class SeatMapWidgetNumberBox extends MapWidgetNumberBox {
         sendStatusChange(MapEventPropagation.DOWNSTREAM, "changed");
         if (this.getChangeRepeat() <= 1) {
             onValueChangeStart();
-            editCtr = PREVIEW_RATE;
-        }
-        if (++editCtr >= PREVIEW_RATE) {
-            editCtr = 0;
-            menu.previewViewer();
         }
     }
 
