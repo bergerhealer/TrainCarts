9029ac3b029b6c6b774efc13539fa9b489cef99d
Sander Jochems <sanderjochems@hotmail.nl>
2020-03-08 22:13:22 +0100

    Implement Vault into SignActionTicket.java

--- src/main/java/com/bergerkiller/bukkit/tc/signactions/SignActionTicket.java
+++ src/main/java/com/bergerkiller/bukkit/tc/signactions/SignActionTicket.java
@@ -1,30 +1,33 @@
 package com.bergerkiller.bukkit.tc.signactions;
 
+import com.bergerkiller.bukkit.common.utils.ParseUtil;
+import com.bergerkiller.bukkit.tc.Localization;
 import com.bergerkiller.bukkit.tc.Permission;
-import com.bergerkiller.bukkit.tc.TCConfig;
+import com.bergerkiller.bukkit.tc.TrainCarts;
+import com.bergerkiller.bukkit.tc.controller.MinecartMember;
 import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.events.SignChangeActionEvent;
-/*
-import com.earth2me.essentials.register.payment.Method;
-import com.earth2me.essentials.register.payment.Method.MethodAccount;
-import com.earth2me.essentials.register.payment.Methods;
-*/
+import org.bukkit.entity.Player;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author reeZZer
- *         Me (Bergerkiller) gives a big thank-you to him for writing the economics for this feature :)
+ * Me (Bergerkiller) gives a big thank-you to him for writing the economics for this feature :)
  */
 public class SignActionTicket extends SignAction {
 
     @Override
     public boolean match(SignActionEvent info) {
-        return TCConfig.EssentialsEnabled && info.isType("ticket");
+        return info.isType("ticket") && info.getMode() != SignActionMode.NONE;
     }
 
     @Override
     public void execute(SignActionEvent info) {
-        throw new RuntimeException("BROKEN");
-        /*
+        if (TrainCarts.getEconomy() == null) {
+            return;
+        }
+
         final boolean isTrain;
         if (info.isCartSign() && info.isAction(SignActionType.MEMBER_ENTER, SignActionType.REDSTONE_ON)) {
             isTrain = false;
@@ -33,52 +36,46 @@ public class SignActionTicket extends SignAction {
         } else {
             return;
         }
-        if ((info.hasMember() && info.isPowered())) {
-            Method method = Methods.hasMethod() ? Methods.getMethod() : null;
-            if (method != null) {
-                double money;
-                if (info.getLine(3).isEmpty()) {
-                    money = 30.0;
-                } else {
-                    money = ParseUtil.parseDouble(info.getLine(3), 0.0);
-                }
-                final String mode = info.getLine(2);
-                List<MinecartMember<?>> members;
-                if (isTrain) {
-                    members = info.getGroup();
-                } else {
-                    members = new ArrayList<>(1);
-                    members.add(info.getMember());
-                }
-                for (MinecartMember<?> member : members) {
-                    if (!member.getEntity().hasPlayerPassenger()) {
-                        continue;
-                    }
-                    Player player = member.getEntity().getPlayerPassenger();
-                    if (!method.hasAccount(player.getName())) {
-                        continue;
-                    }
-                    //actually perform something with it here
-                    MethodAccount account = method.getAccount(player.getName());
 
+        if ((info.hasMember() && info.isPowered())) {
+            double money;
+            if (info.getLine(3).isEmpty()) {
+                money = 30.0;
+            } else {
+                money = ParseUtil.parseDouble(info.getLine(3), 0.0);
+            }
+            final String mode = info.getLine(2);
+
+            List<MinecartMember<?>> members;
+            if (isTrain) {
+                members = info.getGroup();
+            } else {
+                members = new ArrayList<>(1);
+                members.add(info.getMember());
+            }
+
+            for (MinecartMember<?> member : members) {
+                if (!member.getEntity().hasPlayerPassenger()) {
+                    continue;
+                }
+
+                for (Player player : member.getEntity().getPlayerPassengers()) {
                     if (mode.equalsIgnoreCase("add")) {
-                        account.add(money);
-                        Localization.TICKET_ADD.message(player, getCurrencyText(money));
+                        TrainCarts.getEconomy().depositPlayer(player, money);
                     } else if (mode.equalsIgnoreCase("check")) {
-                        Localization.TICKET_CHECK.message(player, getCurrencyText(account.balance()));
+                        Localization.TICKET_CHECK.message(player, TrainCarts.getCurrencyText(TrainCarts.getEconomy().getBalance(player)));
                     } else if (mode.equalsIgnoreCase("buy")) {
-                        if (account.hasUnder(money)) {
-                            Localization.TICKET_BUYFAIL.message(player, getCurrencyText(money));
-                            member.eject();
+                        if (TrainCarts.getEconomy().has(player, money)) {
+                            TrainCarts.getEconomy().withdrawPlayer(player, money);
+                            Localization.TICKET_BUY.message(player, TrainCarts.getCurrencyText(money));
                         } else {
-                            account.subtract(money);
-                            Localization.TICKET_BUY.message(player, getCurrencyText(money));
+                            Localization.TICKET_BUYFAIL.message(player, TrainCarts.getCurrencyText(money));
+                            member.eject();
                         }
                     }
                 }
             }
         }
-        */
     }
 
     @Override
