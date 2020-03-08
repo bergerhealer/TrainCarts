da938af4c111f8aa7089b2baaef3ae7d5fb12244
Sander Jochems <sanderjochems@hotmail.nl>
2020-03-08 20:52:27 +0100

    Make econ static in TrainCarts.java

--- src/main/java/com/bergerkiller/bukkit/tc/TrainCarts.java
+++ src/main/java/com/bergerkiller/bukkit/tc/TrainCarts.java
@@ -76,7 +76,7 @@ public class TrainCarts extends PluginBase {
     private RedstoneTracker redstoneTracker;
     private GlowColorTeamProvider glowColorTeamProvider;
     private PathProvider pathProvider;
-    private Economy econ;
+    private static Economy econ;
 
     /**
      * Gets a helper class for assigning (fake) entities to teams to change their glowing effect
@@ -139,7 +139,7 @@ public class TrainCarts extends PluginBase {
      *
      * @return
      */
-    public Economy getEconomy() {
+    public static Economy getEconomy() {
         return econ;
     }
 
