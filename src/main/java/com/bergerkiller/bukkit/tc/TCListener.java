6f05888a640d6ae7c027bbf12dea3642d7dd7036
Andre_601 <11576465+Andre601@users.noreply.github.com>
2020-07-01 14:41:17 +0200

    Use ISAIR Util method.
    PR: https://github.com/bergerhealer/BKCommonLib/pull/100

--- src/main/java/com/bergerkiller/bukkit/tc/TCListener.java
+++ src/main/java/com/bergerkiller/bukkit/tc/TCListener.java
@@ -630,7 +630,7 @@ public class TCListener implements Listener {
             // If the block below is air or rail, and above is a solid
             Block below = placedBlock.getRelative(BlockFace.DOWN);
             Block above = placedBlock.getRelative(BlockFace.UP);
-            if ((below.getType() == Material.AIR || Util.ISVERTRAIL.get(below)) && BlockUtil.isSuffocating(above)) {
+            if ((Util.ISAIR.get(below) || Util.ISVERTRAIL.get(below)) && BlockUtil.isSuffocating(above)) {
 
                 // Custom placement of an upside-down normal rail
                 BlockPlaceEvent placeEvent = new BlockPlaceEvent(placedBlock, placedBlock.getState(),
