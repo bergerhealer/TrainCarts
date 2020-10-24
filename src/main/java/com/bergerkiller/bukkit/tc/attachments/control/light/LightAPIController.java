8b438e9d1daf309ecc39bc84498ed9e5755fd09f
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2020-10-24 20:23:50 +0200

    Rewrite of lightapi controller to better deal with changing light level of a single block

--- src/main/java/com/bergerkiller/bukkit/tc/attachments/control/light/LightAPIController.java
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/control/light/LightAPIController.java
@@ -1,9 +1,7 @@
 package com.bergerkiller.bukkit.tc.attachments.control.light;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,8 +10,6 @@ import org.bukkit.World;
 import com.bergerkiller.bukkit.common.Task;
 import com.bergerkiller.bukkit.common.bases.IntVector3;
 import com.bergerkiller.bukkit.tc.TrainCarts;
-import com.google.common.collect.LinkedListMultimap;
-import com.google.common.collect.ListMultimap;
 
 import ru.beykerykt.lightapi.LightAPI;
 import ru.beykerykt.lightapi.LightType;
@@ -67,30 +63,31 @@ public class LightAPIController {
 
     private final World world;
     private final LightType lightType;
-    private final ListMultimap<IntVector3, Integer> states;
-    private final Map<IntVector3, List<Integer>> dirty;
+    private final Map<IntVector3, LevelList> levels;
+    private final Map<IntVector3, LevelList> dirty;
     private boolean syncPending;
 
     private LightAPIController(World world, LightType lightType) {
         this.world = world;
         this.lightType = lightType;
-        this.states = LinkedListMultimap.create(100);
+        this.levels = new HashMap<>();
         this.dirty = new HashMap<>();
         this.syncPending = false;
     }
 
     public void add(IntVector3 position, int level) {
-        List<Integer> list = states.get(position);
-        if (list.isEmpty() || Collections.max(list).intValue() < level) {
-            dirty.put(position, list);
-            schedule(this);
+        if (level >= 1 && level <= 15) {
+            LevelList list = levels.computeIfAbsent(position, p -> new LevelList());
+            if (list.add(level)) {
+                dirty.put(position, list);
+                schedule(this);
+            }
         }
-        list.add(Integer.valueOf(level));
     }
 
     public void remove(IntVector3 position, int level) {
-        List<Integer> list = states.get(position);
-        if (list.remove(Integer.valueOf(level)) && (list.isEmpty() || Collections.max(list).intValue() < level)) {
+        LevelList list = levels.get(position);
+        if (list != null && list.remove(level)) {
             dirty.put(position, list);
             schedule(this);
         }
@@ -110,9 +107,8 @@ public class LightAPIController {
         Set<ChunkInfo> chunks = new HashSet<>();
 
         // First process all dirty entries that need light removed
-        for (Map.Entry<IntVector3, List<Integer>> dirty_entry : dirty.entrySet()) {
-            List<Integer> levels = dirty_entry.getValue();
-            if (levels.isEmpty()) {
+        for (Map.Entry<IntVector3, LevelList> dirty_entry : dirty.entrySet()) {
+            if (dirty_entry.getValue().needsRemoving()) {
                 IntVector3 pos = dirty_entry.getKey();
                 LightAPI.deleteLight(world, pos.x, pos.y, pos.z, lightType, async);
                 chunks.addAll(LightAPI.collectChunks(world, pos.x, pos.y, pos.z, lightType, 15));
@@ -120,11 +116,15 @@ public class LightAPIController {
         }
 
         // Then process the dirty entries that create new light
-        for (Map.Entry<IntVector3, List<Integer>> dirty_entry : dirty.entrySet()) {
-            List<Integer> levels = dirty_entry.getValue();
-            if (!levels.isEmpty()) {
+        for (Map.Entry<IntVector3, LevelList> dirty_entry : dirty.entrySet()) {
+            LevelList list = dirty_entry.getValue();
+            if (list.isEmpty()) {
+                // Cleanup from states map
+                levels.remove(dirty_entry.getKey());
+            } else {
+                // Sync it up
                 IntVector3 pos = dirty_entry.getKey();
-                int level = Collections.max(dirty_entry.getValue()).intValue();
+                int level = list.sync();
                 LightAPI.createLight(world, pos.x, pos.y, pos.z, lightType, level, async);
                 chunks.addAll(LightAPI.collectChunks(world, pos.x, pos.y, pos.z, lightType, level));
             }
@@ -159,6 +159,149 @@ public class LightAPIController {
             } else if (++ticksIdle > 100) {
                 stop();
                 _task = null;
+            }
+        }
+    }
+
+    /**
+     * List of light levels configured for a given block
+     */
+    private static final class LevelList {
+        private static final int[] NO_LEVELS = new int[0];
+        private static final int[][] SINGLE_LEVEL = new int[16][1];
+        static {
+            for (int level = 0; level <= 15; level++) {
+                SINGLE_LEVEL[level][0] = level;
+            }
+        }
+
+        // Light level that was last synchronized
+        private int sync = 0;
+        // Sorted list of light levels, highest value first
+        private int[] levels = NO_LEVELS;
+
+        /**
+         * Gets whether the original light source needs to be removed
+         * to properly update the light
+         * 
+         * @return True if the previous light needs to be removed
+         */
+        public boolean needsRemoving() {
+            return (levels == NO_LEVELS) ? (sync > 0) : (sync > levels[0]);
+        }
+
+        /**
+         * Gets whether the list of levels is empty.
+         * An empty list results in no light sources being created.
+         * 
+         * @return True if empty
+         */
+        public boolean isEmpty() {
+            return levels == NO_LEVELS;
+        }
+
+        /**
+         * Synchronizes the light level, returning the new maximum light level
+         * applied. Should not be called when empty.
+         * 
+         * @return light level
+         */
+        public int sync() {
+            return sync = levels[0];
+        }
+
+        /**
+         * Adds a new light level to this list. Returns true
+         * if the maximum light level stored has increased as a
+         * result.
+         * 
+         * @param level Light level to add
+         * @return True if the maximum light level has increased as a result
+         */
+        public boolean add(int level) {
+            if (levels == NO_LEVELS) {
+                levels = SINGLE_LEVEL[level];
+                return true;
+            } else if (level > levels[0]) {
+                int[] new_levels = new int[levels.length + 1];
+                new_levels[0] = level;
+                System.arraycopy(levels, 0, new_levels, 1, levels.length);
+                levels = new_levels;
+                return true;
+            } else {
+                int[] new_levels = new int[levels.length + 1];
+                for (int i = 0; i < levels.length; i++) {
+                    int other_level = levels[i];
+                    if (level <= other_level) {
+                        new_levels[i] = other_level;
+                        continue;
+                    }
+
+                    // Level must be put here, in place of a previous level
+                    // Copy all remaining values after
+                    new_levels[i] = level;
+                    System.arraycopy(levels, i, new_levels, i+1, levels.length - i);
+                    levels = new_levels;
+                    return false;
+                }
+
+                // Add level at the end of the list
+                new_levels[levels.length] = level;
+                levels = new_levels;
+                return false;
+            }
+        }
+
+        /**
+         * Removes a single light level from this list. Returns true if the maximum
+         * light level has gone down as a result of removing it.
+         * 
+         * @param level Light level to remove
+         * @return True if the maximum light level dropped
+         */
+        public boolean remove(int level) {
+            int len = levels.length;
+            if (len == 1) {
+                // Optimized for if only one level is stored
+                if (levels[0] == level) {
+                    levels = NO_LEVELS;
+                    return true;
+                } else {
+                    return false;
+                }
+            } else if (len == 2) {
+                // Switch to a single by-level, when two values are stored
+                if (levels[1] == level) {
+                    levels = SINGLE_LEVEL[levels[0]];
+                    return false;
+                } else if (levels[0] == level) {
+                    levels = SINGLE_LEVEL[levels[1]];
+                    return true;
+                } else {
+                    return false;
+                }
+            } else if (levels[0] == level) {
+                // List of levels, and the highest light level was removed
+                // If the level following it is lower, then the light level changed
+                int[] new_levels = new int[len - 1];
+                System.arraycopy(levels, 1, new_levels, 0, len - 1);
+                levels = new_levels;
+                return new_levels[0] != level;
+            } else {
+                // 3 or more requires list remove logic in the middle of the list
+                // The maximum light level doesn't change
+                for (int i = 1; i < len; len++) {
+                    if (levels[i] == level) {
+                        int[] new_levels = new int[len - 1];
+                        System.arraycopy(levels, 0, new_levels, 0, i);
+                        System.arraycopy(levels, i+1, new_levels, i, len - i - 1);
+                        levels = new_levels;
+                        return false;
+                    }
+                }
+
+                // Not found
+                return false;
             }
         }
     }
