6c1bf3dfacba75e83001c9c01d6d3c6e1f68e706
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2020-12-29 01:05:05 +0100

    Add wait property commands for delay, distance and acceleration

--- src/main/java/com/bergerkiller/bukkit/tc/properties/standard/category/WaitOptionsProperty.java
+++ src/main/java/com/bergerkiller/bukkit/tc/properties/standard/category/WaitOptionsProperty.java
@@ -2,13 +2,24 @@ package com.bergerkiller.bukkit.tc.properties.standard.category;
 
 import java.util.Optional;
 
+import org.bukkit.command.CommandSender;
+
 import com.bergerkiller.bukkit.common.config.ConfigurationNode;
 import com.bergerkiller.bukkit.tc.Util;
+import com.bergerkiller.bukkit.tc.commands.annotations.CommandTargetTrain;
+import com.bergerkiller.bukkit.tc.commands.parsers.AccelerationParser;
+import com.bergerkiller.bukkit.tc.properties.TrainProperties;
+import com.bergerkiller.bukkit.tc.properties.api.PropertyCheckPermission;
 import com.bergerkiller.bukkit.tc.properties.api.PropertyInvalidInputException;
 import com.bergerkiller.bukkit.tc.properties.api.PropertyParseContext;
 import com.bergerkiller.bukkit.tc.properties.api.PropertyParser;
 import com.bergerkiller.bukkit.tc.properties.standard.fieldbacked.FieldBackedStandardTrainProperty;
 import com.bergerkiller.bukkit.tc.properties.standard.type.WaitOptions;
+
+import cloud.commandframework.annotations.Argument;
+import cloud.commandframework.annotations.CommandDescription;
+import cloud.commandframework.annotations.CommandMethod;
+import net.md_5.bungee.api.ChatColor;
 
 /**
  * Stores the wait options for a train. This controls whether trains wait
@@ -16,7 +27,101 @@ import com.bergerkiller.bukkit.tc.properties.standard.type.WaitOptions;
  */
 public final class WaitOptionsProperty extends FieldBackedStandardTrainProperty<WaitOptions> {
 
-    @PropertyParser("waitdistance")
+    @CommandTargetTrain
+    @PropertyCheckPermission("waitdistance")
+    @CommandMethod("train wait distance <blocks>")
+    @CommandDescription("Sets the distance to keep to other trains")
+    private void setDistanceProperty(
+            final CommandSender sender,
+            final TrainProperties properties,
+            final @Argument(value="blocks", description="Number of blocks distance") double distance
+    ) {
+        properties.update(this, options -> WaitOptions.create(
+                distance,
+                options.delay(),
+                options.acceleration(),
+                options.deceleration()));
+
+        getDistanceProperty(sender, properties);
+    }
+
+    @CommandMethod("train wait distance")
+    @CommandDescription("Displays the distance to keep to other trains")
+    private void getDistanceProperty(
+            final CommandSender sender,
+            final TrainProperties properties
+    ) {
+        sender.sendMessage(ChatColor.YELLOW + "Distance to keep to other trains: "
+                + ChatColor.GREEN + properties.getWaitDistance() + " blocks");
+    }
+
+    @CommandTargetTrain
+    @PropertyCheckPermission("waitdelay")
+    @CommandMethod("train wait delay <time>")
+    @CommandDescription("Sets the time a train waits when fully stopped to wait")
+    private void setDelayProperty(
+            final CommandSender sender,
+            final TrainProperties properties,
+            final @Argument(value="time", description="Time to wait in seconds") double delay
+    ) {
+        properties.update(this, options -> WaitOptions.create(
+                options.distance(),
+                delay,
+                options.acceleration(),
+                options.deceleration()));
+
+        getDelayProperty(sender, properties);
+    }
+
+    @CommandMethod("train wait delay")
+    @CommandDescription("Displays the time a train waits when fully stopped to wait")
+    private void getDelayProperty(
+            final CommandSender sender,
+            final TrainProperties properties
+    ) {
+        sender.sendMessage(ChatColor.YELLOW + "Wait delay when stopped: "
+                + ChatColor.GREEN + properties.getWaitDelay() + " seconds");
+    }
+
+    @CommandTargetTrain
+    @PropertyCheckPermission("waitacceleration")
+    @CommandMethod("train wait acceleration <acceleration> [deceleration]")
+    @CommandDescription("Sets the rate of acceleration (and deceleration) of the train")
+    private void setAccelerationProperty(
+            final CommandSender sender,
+            final TrainProperties properties,
+            final @Argument(value="acceleration", parserName=AccelerationParser.NAME,
+                            description="Acceleration in blocks/tick\u00B2") double acceleration,
+            final @Argument(value="deceleration", parserName=AccelerationParser.NAME,
+                            defaultValue="NaN", description="De-acceleration in blocks/tick\u00B2") double deceleration
+    ) {
+        properties.update(this, options -> WaitOptions.create(
+                options.distance(),
+                options.delay(),
+                acceleration,
+                Double.isNaN(deceleration) ? acceleration : deceleration));
+
+        getAccelerationProperty(sender, properties);
+    }
+
+    @CommandMethod("train wait acceleration")
+    @CommandDescription("Displays the rate of acceleration (and deceleration) of the train")
+    private void getAccelerationProperty(
+            final CommandSender sender,
+            final TrainProperties properties
+    ) {
+        if (properties.getWaitAcceleration() == properties.getWaitDeceleration()) {
+            sender.sendMessage(ChatColor.YELLOW + "Speeds up and slows down to wait at: "
+                    + ChatColor.GREEN + properties.getWaitAcceleration() + " blocks/tick\u00B2");
+        } else {
+            sender.sendMessage(ChatColor.YELLOW + "Slows down to wait at: "
+                    + ChatColor.GREEN + properties.getWaitDeceleration() + " blocks/tick\u00B2");
+            sender.sendMessage(ChatColor.YELLOW + "Speeds up after waiting at: "
+                    + ChatColor.GREEN + properties.getWaitAcceleration() + " blocks/tick\u00B2");
+        }
+    }
+
+    @PropertyParser("waitdistance|wait distance")
     public WaitOptions parseWaitDistance(PropertyParseContext<WaitOptions> context) {
         return WaitOptions.create(context.inputDouble(),
                 context.current().delay(),
@@ -24,7 +129,7 @@ public final class WaitOptionsProperty extends FieldBackedStandardTrainProperty<
                 context.current().deceleration());
     }
 
-    @PropertyParser("waitdelay")
+    @PropertyParser("waitdelay|wait delay")
     public WaitOptions parseWaitDelay(PropertyParseContext<WaitOptions> context) {
         return WaitOptions.create(context.current().distance(),
                 context.inputDouble(),
@@ -32,7 +137,7 @@ public final class WaitOptionsProperty extends FieldBackedStandardTrainProperty<
                 context.current().deceleration());
     }
 
-    @PropertyParser("waitacceleration")
+    @PropertyParser("waitacceleration|wait acceleration")
     public WaitOptions parseWaitAcceleration(PropertyParseContext<WaitOptions> context) {
         String[] args = context.input().trim().split(" ");
         double newAcceleration;
