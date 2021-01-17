e56eae056df763b080fa2ac8193054c73572b55c
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2021-01-17 03:07:01 +0100

    Remove unused import

--- src/main/java/com/bergerkiller/bukkit/tc/commands/selector/SelectorHandlerRegistry.java
+++ src/main/java/com/bergerkiller/bukkit/tc/commands/selector/SelectorHandlerRegistry.java
@@ -20,7 +20,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.server.ServerCommandEvent;
 
 import com.bergerkiller.generated.net.minecraft.server.PlayerConnectionHandle;
