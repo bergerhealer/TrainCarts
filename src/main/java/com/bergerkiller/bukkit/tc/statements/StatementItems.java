d4c27ecc92cd15cb2d6ddf930115a3cc35a277dd
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2021-03-22 18:43:37 +0100

    Treat null inventory as 0-slot inventory to allow negation to work right

--- src/main/java/com/bergerkiller/bukkit/tc/statements/StatementItems.java
+++ src/main/java/com/bergerkiller/bukkit/tc/statements/StatementItems.java
@@ -1,6 +1,7 @@
 package com.bergerkiller.bukkit.tc.statements;
 
 import com.bergerkiller.bukkit.common.inventory.ItemParser;
+import com.bergerkiller.bukkit.common.inventory.MergedInventory;
 import com.bergerkiller.bukkit.common.utils.ItemUtil;
 import com.bergerkiller.bukkit.tc.Util;
 import com.bergerkiller.bukkit.tc.controller.MinecartGroup;
@@ -33,9 +34,9 @@ public abstract class StatementItems extends Statement {
 
     @Override
     public boolean handle(MinecartMember<?> member, String text, SignActionEvent event) {
-        final Inventory inventory = getInventory(member);
+        Inventory inventory = getInventory(member);
         if (inventory == null) {
-            return false;
+            inventory = new MergedInventory(); // simulate empty inventory
         }
         int count = ItemUtil.getItemCount(getInventory(member), null, -1);
         return Util.evaluate(count, text);
@@ -43,9 +44,9 @@ public abstract class StatementItems extends Statement {
 
     @Override
     public boolean handle(MinecartGroup group, String text, SignActionEvent event) {
-        final Inventory inventory = getInventory(group);
+        Inventory inventory = getInventory(group);
         if (inventory == null) {
-            return false;
+            inventory = new MergedInventory(); // simulate empty inventory
         }
         int count = ItemUtil.getItemCount(inventory, null, -1);
         return Util.evaluate(count, text);
@@ -55,37 +56,45 @@ public abstract class StatementItems extends Statement {
         if (inv == null) {
             return false;
         }
-        int opidx;
-        int count;
+
         for (String itemname : items) {
-            opidx = Util.getOperatorIndex(itemname);
-            String itemnamefixed;
-            if (opidx > 0) {
-                itemnamefixed = itemname.substring(0, opidx);
+            int count;
+            if (inv.getSize() == 0) {
+                // Zero-slot inventory, no need to search anything
+                count = 0;
             } else {
-                itemnamefixed = itemname;
-            }
-            for (ItemParser parser : Util.getParsers(itemnamefixed)) {
-                count = ItemUtil.getItemCount(inv, parser.getType(), parser.getData());
-                if (opidx == -1) {
-                    if (parser.hasAmount()) {
-                        if (count >= parser.getAmount()) {
+                // Search and count the items in the inventory
+                int opidx = Util.getOperatorIndex(itemname);
+                String itemnamefixed;
+                if (opidx > 0) {
+                    itemnamefixed = itemname.substring(0, opidx);
+                } else {
+                    itemnamefixed = itemname;
+                }
+                for (ItemParser parser : Util.getParsers(itemnamefixed)) {
+                    count = ItemUtil.getItemCount(inv, parser.getType(), parser.getData());
+                    if (opidx == -1) {
+                        if (parser.hasAmount()) {
+                            if (count >= parser.getAmount()) {
+                                return true;
+                            }
+                        } else if (count > 0) {
                             return true;
                         }
-                    } else if (count > 0) {
+                    } else if (Util.evaluate(count, itemname)) {
                         return true;
                     }
-                } else if (Util.evaluate(count, itemname)) {
-                    return true;
+                }
+
+                // Check for 'special' named items
+                count = 0;
+                for (ItemStack item : inv) {
+                    if (item != null && ItemUtil.hasDisplayName(item) && ItemUtil.getDisplayName(item).equals(itemnamefixed)) {
+                        count += item.getAmount();
+                    }
                 }
             }
-            // Check for 'special' named items
-            count = 0;
-            for (ItemStack item : inv) {
-                if (item != null && ItemUtil.hasDisplayName(item) && ItemUtil.getDisplayName(item).equals(itemnamefixed)) {
-                    count += item.getAmount();
-                }
-            }
+
             if (Util.evaluate(count, itemname)) {
                 return true;
             }
