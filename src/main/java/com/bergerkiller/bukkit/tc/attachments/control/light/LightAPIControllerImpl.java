076d57e4e5c8266cccb0329f57d40a4adc3e087a
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2021-08-23 18:40:36 +0200

    Working towards 1.17.1-v2: Better handle LightAPI breaking

--- src/main/java/com/bergerkiller/bukkit/tc/attachments/control/light/LightAPIControllerImpl.java
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/control/light/LightAPIControllerImpl.java
@@ -1,118 +1,103 @@
 package com.bergerkiller.bukkit.tc.attachments.control.light;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.logging.Level;
+import java.util.Set;
 
-import org.bukkit.Bukkit;
 import org.bukkit.World;
-import org.bukkit.plugin.Plugin;
 
-import com.bergerkiller.bukkit.common.Task;
 import com.bergerkiller.bukkit.common.bases.IntVector3;
-import com.bergerkiller.bukkit.tc.TrainCarts;
 
-public abstract class LightAPIController {
-    private static final Map<World, LightAPIController> _blockLightControllers = new HashMap<>();
-    private static final Map<World, LightAPIController> _skyLightControllers = new HashMap<>();
-    private static SyncTask _task;
-    private boolean syncPending;
+import ru.beykerykt.lightapi.LightAPI;
+import ru.beykerykt.lightapi.LightType;
+import ru.beykerykt.lightapi.chunks.ChunkInfo;
 
-    protected LightAPIController() {
-        syncPending = false;
+/**
+ * Implementation of the LightAPIController. Contains LightAPI
+ * types.
+ */
+public class LightAPIControllerImpl extends LightAPIController {
+    private final World world;
+    private final LightType lightType;
+    private final Map<IntVector3, LevelList> levels;
+    private final Map<IntVector3, LevelList> dirty;
+
+    public static LightAPIController forSkyLight(World world) {
+        return new LightAPIControllerImpl(world, LightType.SKY);
     }
 
-    protected void schedule() {
-        if (!syncPending) {
-            syncPending = true;
-            if (_task == null) {
-                _task = new SyncTask();
-                if (_task.getPlugin().isEnabled()) {
-                    _task.start(1, 1);
-                }
+    public static LightAPIController forBlockLight(World world) {
+        return new LightAPIControllerImpl(world, LightType.BLOCK);
+    }
+
+    private LightAPIControllerImpl(World world, LightType lightType) {
+        this.world = world;
+        this.lightType = lightType;
+        this.levels = new HashMap<>();
+        this.dirty = new HashMap<>();
+    }
+
+    public void add(IntVector3 position, int level) {
+        if (level >= 1 && level <= 15) {
+            LevelList list = levels.computeIfAbsent(position, p -> new LevelList());
+            if (list.add(level)) {
+                dirty.put(position, list);
+                schedule();
             }
         }
     }
 
-    /**
-     * Gets a controller instance for a World
-     * 
-     * @param world The world on which light is manipulated
-     * @param skyLight Whether to change the sky light (true) or block light (false)
-     * @return controller
-     */
-    public static LightAPIController get(World world, boolean skyLight) {
-        Map<World, LightAPIController> map = skyLight ? _skyLightControllers : _blockLightControllers;
-        LightAPIController controller = map.get(world);
-        if (controller == null) {
-            try {
-                controller = skyLight ? LightAPIControllerImpl.forSkyLight(world) : LightAPIControllerImpl.forBlockLight(world);
-            } catch (Throwable t) {
-                Plugin plugin = Bukkit.getPluginManager().getPlugin("LightAPI");
-                if (plugin == null) {
-                    // Not loaded
-                    TrainCarts.plugin.getLogger().log(Level.SEVERE, "Failed to initialize LightAPI handler: LightAPI plugin is not enabled!");
-                } else if (plugin.getDescription().getMain().equals("ru.beykerykt.minecraft.lightapi.bukkit.impl.BukkitPlugin")) {
-                    // LightAPI is used instead of LightAPI-Fork - not supported!
-                    TrainCarts.plugin.getLogger().log(Level.SEVERE, "Failed to initialize LightAPI handler: LightAPI is installed, but you need LightAPI-fork instead!");
-                } else {
-                    TrainCarts.plugin.getLogger().log(Level.SEVERE, "Failed to initialize LightAPI handler", t);
-                }
-                controller = LightAPIControllerUnavailable.INSTANCE;
-            }
-            map.put(world, controller);
+    public void remove(IntVector3 position, int level) {
+        LevelList list = levels.get(position);
+        if (list != null && list.remove(level)) {
+            dirty.put(position, list);
+            schedule();
         }
-        return controller;
     }
 
-    public static void disableWorld(World world) {
-        _blockLightControllers.remove(world);
-        _skyLightControllers.remove(world);
+    public void move(IntVector3 old_position, IntVector3 new_position, int level) {
+        remove(old_position, level);
+        add(new_position, level);
     }
 
-    public static void disable() {
-        _blockLightControllers.clear();
-        _skyLightControllers.clear();
-        Task.stop(_task);
-        _task = null;
-    }
+    public boolean onSync() {
+        if (dirty.isEmpty()) {
+            return false;
+        }
+        final boolean async = true;
+        Set<ChunkInfo> chunks = new HashSet<>();
 
-    public abstract void add(IntVector3 position, int level);
-
-    public abstract void remove(IntVector3 position, int level);
-
-    public abstract void move(IntVector3 old_position, IntVector3 new_position, int level);
-
-    protected abstract boolean onSync();
-
-    public final boolean sync() {
-        syncPending = false;
-        return onSync();
-    }
-
-    private static class SyncTask extends Task {
-        private int ticksIdle = 0;
-
-        public SyncTask() {
-            super(TrainCarts.plugin);
+        // First process all dirty entries that need light removed
+        for (Map.Entry<IntVector3, LevelList> dirty_entry : dirty.entrySet()) {
+            if (dirty_entry.getValue().needsRemoving()) {
+                IntVector3 pos = dirty_entry.getKey();
+                LightAPI.deleteLight(world, pos.x, pos.y, pos.z, lightType, async);
+                chunks.addAll(LightAPI.collectChunks(world, pos.x, pos.y, pos.z, lightType, 15));
+            }
         }
 
-        @Override
-        public void run() {
-            boolean busy = false;
-            for (LightAPIController controller : _blockLightControllers.values()) {
-                busy |= controller.sync();
-            }
-            for (LightAPIController controller : _skyLightControllers.values()) {
-                busy |= controller.sync();
-            }
-            if (busy) {
-                ticksIdle = 0;
-            } else if (++ticksIdle > 100) {
-                stop();
-                _task = null;
+        // Then process the dirty entries that create new light
+        for (Map.Entry<IntVector3, LevelList> dirty_entry : dirty.entrySet()) {
+            LevelList list = dirty_entry.getValue();
+            if (list.isEmpty()) {
+                // Cleanup from states map
+                levels.remove(dirty_entry.getKey());
+            } else {
+                // Sync it up
+                IntVector3 pos = dirty_entry.getKey();
+                int level = list.sync();
+                LightAPI.createLight(world, pos.x, pos.y, pos.z, lightType, level, async);
+                chunks.addAll(LightAPI.collectChunks(world, pos.x, pos.y, pos.z, lightType, level));
             }
         }
+
+        // Refresh chunks and done
+        dirty.clear();
+        for (ChunkInfo chunk : chunks) {
+            LightAPI.updateChunk(chunk, lightType);
+        }
+        return true;
     }
 
     /**
---
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/control/light/LightAPIControllerUnavailable.java
@@ -0,0 +1,27 @@
+package com.bergerkiller.bukkit.tc.attachments.control.light;
+
+import com.bergerkiller.bukkit.common.bases.IntVector3;
+
+public class LightAPIControllerUnavailable extends LightAPIController {
+    public static final LightAPIControllerUnavailable INSTANCE = new LightAPIControllerUnavailable();
+
+    private LightAPIControllerUnavailable() {
+    }
+
+    @Override
+    public void add(IntVector3 position, int level) {
+    }
+
+    @Override
+    public void remove(IntVector3 position, int level) {
+    }
+
+    @Override
+    public void move(IntVector3 old_position, IntVector3 new_position, int level) {
+    }
+
+    @Override
+    protected boolean onSync() {
+        return false;
+    }
+}
