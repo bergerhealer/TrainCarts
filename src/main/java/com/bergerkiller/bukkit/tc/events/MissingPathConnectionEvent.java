d3e3779e442f6b1f954b95acece0278297d307d8
IrgendSoEinTyp <j0schlz@gmx.net>
2021-09-12 20:05:26 +0200

    Added involved RailPiece to MissingPathConnectionEvent

--- src/main/java/com/bergerkiller/bukkit/tc/events/MissingPathConnectionEvent.java
+++ src/main/java/com/bergerkiller/bukkit/tc/events/MissingPathConnectionEvent.java
@@ -1,6 +1,7 @@
 package com.bergerkiller.bukkit.tc.events;
 
 import com.bergerkiller.bukkit.tc.controller.MinecartGroup;
+import com.bergerkiller.bukkit.tc.controller.components.RailPiece;
 import com.bergerkiller.bukkit.tc.pathfinding.PathNode;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -12,16 +13,19 @@ import org.jetbrains.annotations.NotNull;
 public class MissingPathConnectionEvent extends Event {
     private static final HandlerList HANDLERS = new HandlerList();
 
-    protected final PathNode node;
-    private MinecartGroup group;
+    private final RailPiece rail;
+    private final PathNode node;
+    private final MinecartGroup group;
     private final String destination;
 
-    public MissingPathConnectionEvent(PathNode node, MinecartGroup group, String destination) {
+    public MissingPathConnectionEvent(RailPiece rail, PathNode node, MinecartGroup group, String destination) {
+        this.rail = rail;
         this.node = node;
         this.group = group;
         this.destination = destination;
     }
 
+    public RailPiece getRail() { return rail; }
     public PathNode getPathNode() {
         return this.node;
     }
