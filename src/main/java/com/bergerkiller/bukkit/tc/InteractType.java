bd034ebba3689ebbe981b830085fddfd32fb3246
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-07-09 16:13:13 +0200

    optimized InteractType-class

--- src/main/java/com/bergerkiller/bukkit/tc/InteractType.java
+++ src/main/java/com/bergerkiller/bukkit/tc/InteractType.java
@@ -18,59 +18,31 @@ public enum InteractType {
      */
     public static Collection<InteractType> parse(String root, String name) {
         name = name.toLowerCase();
-        LinkedHashSet<InteractType> typesToCheck = new LinkedHashSet<>();
+        final LinkedHashSet<InteractType> typesToCheck = new LinkedHashSet<>();
         if (root.equals("collect")) {
-            if (name.startsWith("chest out")) {
-                typesToCheck.add(CHEST);
-            } else if (name.startsWith("dispenser out")) {
-                typesToCheck.add(DISPENSER);
-            } else if (name.startsWith("dropper out")) {
-                typesToCheck.add(DROPPER);
-            } else if (name.startsWith("furnace out")) {
-                typesToCheck.add(FURNACE);
-            } else if (name.startsWith("pickup") || name.startsWith("pick up")) {
-                typesToCheck.add(GROUNDITEM);
-            }
-        } else if (root.equals("deposit")) {
-            if (name.startsWith("chest in")) {
-                typesToCheck.add(CHEST);
-            } else if (name.startsWith("dispenser in")) {
-                typesToCheck.add(DISPENSER);
-            } else if (name.startsWith("dropper in")) {
-                typesToCheck.add(DROPPER);
-            } else if (name.startsWith("furnace in")) {
-                typesToCheck.add(FURNACE);
-            } else if (name.startsWith("smelt")) {
-                typesToCheck.add(FURNACE);
-            } else if (name.startsWith("drop items") || name.startsWith("dropitems")) {
-                typesToCheck.add(GROUNDITEM);
-            }
-        }
+            if (name.startsWith("chest out")) typesToCheck.add(CHEST);
+            else if (name.startsWith("dispenser out")) typesToCheck.add(DISPENSER);
+            else if (name.startsWith("dropper out")) typesToCheck.add(DROPPER);
+            else if (name.startsWith("furnace out")) typesToCheck.add(FURNACE);
+            else if (name.startsWith("pickup") || name.startsWith("pick up")) typesToCheck.add(GROUNDITEM);
+        } else if (root.equals("deposit")) if (name.startsWith("chest in")) typesToCheck.add(CHEST);
+        else if (name.startsWith("dispenser in")) typesToCheck.add(DISPENSER);
+        else if (name.startsWith("dropper in")) typesToCheck.add(DROPPER);
+        else if (name.startsWith("furnace in")) typesToCheck.add(FURNACE);
+        else if (name.startsWith("smelt")) typesToCheck.add(FURNACE);
+        else if (name.startsWith("drop items") || name.startsWith("dropitems")) typesToCheck.add(GROUNDITEM);
         if (name.startsWith(root + ' ')) {
-            String types = name.substring(root.length() + 1).toLowerCase();
-            if (types.startsWith("chest")) {
-                typesToCheck.add(CHEST);
-            } else if (types.startsWith("furn")) {
-                typesToCheck.add(FURNACE);
-            } else if (types.startsWith("disp")) {
-                typesToCheck.add(DISPENSER);
-            } else if (types.startsWith("drop")) {
-                typesToCheck.add(DROPPER);
-            } else if (types.startsWith("ground")) {
-                typesToCheck.add(GROUNDITEM);
-            } else {
-                for (char c : types.toCharArray()) {
-                    if (c == 'c') {
-                        typesToCheck.add(CHEST);
-                    } else if (c == 'f') {
-                        typesToCheck.add(FURNACE);
-                    } else if (c == 'd') {
-                        typesToCheck.add(DISPENSER);
-                    } else if (c == 'g') {
-                        typesToCheck.add(GROUNDITEM);
-                    }
-                }
-            }
+            final String types = name.substring(root.length() + 1).toLowerCase();
+            if (types.startsWith("chest")) typesToCheck.add(CHEST);
+            else if (types.startsWith("furn")) typesToCheck.add(FURNACE);
+            else if (types.startsWith("disp")) typesToCheck.add(DISPENSER);
+            else if (types.startsWith("drop")) typesToCheck.add(DROPPER);
+            else if (types.startsWith("ground")) typesToCheck.add(GROUNDITEM);
+            else for (char c : types.toCharArray())
+                    if (c == 'c') typesToCheck.add(CHEST);
+                    else if (c == 'f') typesToCheck.add(FURNACE);
+                    else if (c == 'd') typesToCheck.add(DISPENSER);
+                    else if (c == 'g') typesToCheck.add(GROUNDITEM);
         }
         if (name.startsWith(root) && typesToCheck.isEmpty()) {
             typesToCheck.add(CHEST);
