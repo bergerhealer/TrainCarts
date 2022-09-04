402a5e6753122189e8eda386e4427e2df8c4fa65
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2022-09-05 01:35:14 +0200

    [Spawner] Verify sign text is still the same when spawning/placing sign

--- src/main/java/com/bergerkiller/bukkit/tc/signactions/spawner/SpawnSign.java
+++ src/main/java/com/bergerkiller/bukkit/tc/signactions/spawner/SpawnSign.java
@@ -30,6 +30,7 @@ public class SpawnSign {
     private final TrainCarts plugin;
     private final OfflineSignStore store;
     private final OfflineBlock location;
+    private OfflineSign sign;
     private SpawnSignManager.SpawnSignMetadata state;
     private int ticksUntilFreeing = 0;
     private double spawnForce = 0.0;
@@ -41,6 +42,7 @@ public class SpawnSign {
         this.plugin = plugin;
         this.store = store;
         this.location = sign.getBlock();
+        this.sign = sign;
         this.updateState(sign, metadata);
 
         // Add the 5x5 area of chunks around the sign as the initial chunks to load
@@ -68,9 +70,17 @@ public class SpawnSign {
         this.spawnForce = SpawnOptions.fromOfflineSign(sign).launchVelocity;
         this.spawnFormat = sign.getLine(2) + sign.getLine(3);
         this.state = metadata;
+        this.sign = sign;
     }
 
     void updateUsingEvent(SignActionEvent event) {
+        // If known offline sign text mismatches with the event, got to re-add the sign completely.
+        // This calls updateState() after the onUpdated() callback.
+        if (!this.sign.verify(event.getSign())) {
+            this.store.put(event.getBlock(), this.state.setActive(event.isPowered()));
+            return;
+        }
+
         // Update active (redstone) state in case it changed without us knowing
         boolean active = event.isPowered();
         if (active != this.state.active) {
