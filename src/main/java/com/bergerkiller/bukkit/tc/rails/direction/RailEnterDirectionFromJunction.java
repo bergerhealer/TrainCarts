23c3dae483f10b3d395f867d0eaa5061c608b630
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2022-10-03 00:25:32 +0200

    Implement junction enter direction match, bringing it alive

--- src/main/java/com/bergerkiller/bukkit/tc/rails/direction/RailEnterDirectionFromJunction.java
+++ src/main/java/com/bergerkiller/bukkit/tc/rails/direction/RailEnterDirectionFromJunction.java
@@ -3,6 +3,7 @@ package com.bergerkiller.bukkit.tc.rails.direction;
 import org.bukkit.util.Vector;
 
 import com.bergerkiller.bukkit.tc.controller.components.RailJunction;
+import com.bergerkiller.bukkit.tc.controller.components.RailPath;
 import com.bergerkiller.bukkit.tc.controller.components.RailState;
 
 /**
@@ -37,8 +38,25 @@ public final class RailEnterDirectionFromJunction implements RailEnterDirection 
 
     @Override
     public boolean match(RailState state) {
-        // TODO Auto-generated method stub
-        return false;
+        // Take the rail state and walk it backwards until we reach the end of the path
+        // This should be nearly the exact position and direction this junction represents
+        RailPath path = state.loadRailLogic().getPath();
+        RailPath.Position pos = state.position().clone();
+        pos.makeRelative(state.railBlock());
+        pos.invertMotion();
+
+        // Move backwards all the way to the back
+        path.moveRelative(pos, Double.MAX_VALUE);
+
+        // Verify current position has the correct movement direction
+        // This works regardless of whether the junction is absolute or relative, so just do it now
+        if (pos.motDot(junction.position()) <= 0.0) {
+            return false;
+        }
+
+        // Verify the positions are acceptably close together
+        // 1e-5 = (1e-10 * 1e-10)
+        return pos.distanceSquaredAtRail(state.railBlock(), junction.position()) < 1e-10;
     }
 
     @Override
