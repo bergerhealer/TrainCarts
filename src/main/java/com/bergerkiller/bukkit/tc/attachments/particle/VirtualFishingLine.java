0a51b04de4f4baf99abca2b394afc893121ab573
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2023-02-22 02:08:10 +0100

    Fix fishing line not spawning right on 1.10.2 and before

--- src/main/java/com/bergerkiller/bukkit/tc/attachments/particle/VirtualFishingLine.java
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/particle/VirtualFishingLine.java
@@ -26,12 +26,21 @@ import com.bergerkiller.generated.net.minecraft.world.entity.projectile.EntityFi
  * Tracks and updates a single fishing line connecting two points
  */
 public class VirtualFishingLine {
-    /** Offset to position the player so that the held fishing rod line is exactly at 0/0/0 */
-    private static final Vector OFFSET_PLAYER = new Vector(-0.35, -1.17, -0.8);
-    /** Offset to position the silverfish mount of the player to align at 0/0/0 */
-    private static final Vector OFFSET_HOLDER = new Vector(-0.35, -1.04, -0.8);
-    /** Offset to position the hooked silverfish entity to align hook-line at 0/0/0 */
-    private static final Vector OFFSET_HOOKED = new Vector(0.0, - 0.49, 0.0);
+    private static final Offsets OFFSETS_1_8 = new Offsets(
+            /** Offset to position the player so that the held fishing rod line is exactly at 0/0/0 */
+            new Vector(0.35, -1.17, -0.8),
+            /** Offset to position the silverfish mount of the player to align at 0/0/0 */
+            new Vector(0.35, -1.04, -0.8),
+            /** Offset to position the hooked silverfish entity to align hook-line at 0/0/0 */
+            new Vector(0.0, -0.49, 0.0));
+    private static final Offsets OFFSETS_1_11 = new Offsets(
+            /** Offset to position the player so that the held fishing rod line is exactly at 0/0/0 */
+            new Vector(-0.35, -1.17, -0.8),
+            /** Offset to position the silverfish mount of the player to align at 0/0/0 */
+            new Vector(-0.35, -1.04, -0.8),
+            /** Offset to position the hooked silverfish entity to align hook-line at 0/0/0 */
+            new Vector(0.0, -0.49, 0.0));
+
     private final int hookedEntityId, holderEntityId, holderPlayerEntityId, hookEntityId;
 
     public VirtualFishingLine() {
@@ -43,6 +52,14 @@ public class VirtualFishingLine {
         this.holderEntityId = useViewerAsHolder ? -1 : EntityUtil.getUniqueEntityId();
         this.holderPlayerEntityId = useViewerAsHolder ? -1 : EntityUtil.getUniqueEntityId();
         this.hookEntityId = EntityUtil.getUniqueEntityId();
+    }
+
+    private Offsets offsets(AttachmentViewer viewer) {
+        if (viewer.evaluateGameVersion(">=", "1.11")) {
+            return OFFSETS_1_11;
+        } else {
+            return OFFSETS_1_8;
+        }
     }
 
     /**
@@ -84,13 +101,15 @@ public class VirtualFishingLine {
     }
 
     void spawnWithoutLineCollectUUIDs(AttachmentViewer viewer, Vector positionA, Vector positionB, List<UUID> uuids) {
+        final Offsets OFFSET = offsets(viewer);
+
         // Spawn the invisible entity that holds the other end of the fishing hook
         // Seems that this must be a player entity, so just spawn clones of the viewer
         if (this.holderPlayerEntityId != -1) {
             FakePlayerSpawner.NO_NAMETAG_RANDOM.spawnPlayerSimple(viewer, viewer.getPlayer(), this.holderPlayerEntityId, spawnPacket -> {
-                spawnPacket.setPosX(positionA.getX() + OFFSET_PLAYER.getX());
-                spawnPacket.setPosY(positionA.getY() + OFFSET_PLAYER.getY());
-                spawnPacket.setPosZ(positionA.getZ() + OFFSET_PLAYER.getZ());
+                spawnPacket.setPosX(positionA.getX() + OFFSET.PLAYER.getX());
+                spawnPacket.setPosY(positionA.getY() + OFFSET.PLAYER.getY());
+                spawnPacket.setPosZ(positionA.getZ() + OFFSET.PLAYER.getZ());
             }, meta -> {
                 meta.set(EntityHandle.DATA_NO_GRAVITY, true);
                 meta.setFlag(EntityHandle.DATA_FLAGS, EntityHandle.DATA_FLAG_INVISIBLE, true);
@@ -107,9 +126,9 @@ public class VirtualFishingLine {
             spawnPacket.setEntityId(this.holderEntityId);
             spawnPacket.setEntityUUID(uuid);
             spawnPacket.setEntityType(EntityType.SILVERFISH);
-            spawnPacket.setPosX(positionA.getX() + OFFSET_HOLDER.getX());
-            spawnPacket.setPosY(positionA.getY() + OFFSET_HOLDER.getY());
-            spawnPacket.setPosZ(positionA.getZ() + OFFSET_HOLDER.getZ());
+            spawnPacket.setPosX(positionA.getX() + OFFSET.HOLDER.getX());
+            spawnPacket.setPosY(positionA.getY() + OFFSET.HOLDER.getY());
+            spawnPacket.setPosZ(positionA.getZ() + OFFSET.HOLDER.getZ());
 
             DataWatcher meta = new DataWatcher();
             meta.set(EntityHandle.DATA_NO_GRAVITY, true);
@@ -128,9 +147,9 @@ public class VirtualFishingLine {
             spawnPacket.setEntityId(this.hookedEntityId);
             spawnPacket.setEntityUUID(uuid);
             spawnPacket.setEntityType(EntityType.SILVERFISH);
-            spawnPacket.setPosX(positionB.getX() + OFFSET_HOOKED.getX());
-            spawnPacket.setPosY(positionB.getY() + OFFSET_HOOKED.getY());
-            spawnPacket.setPosZ(positionB.getZ() + OFFSET_HOOKED.getZ());
+            spawnPacket.setPosX(positionB.getX() + OFFSET.HOOKED.getX());
+            spawnPacket.setPosY(positionB.getY() + OFFSET.HOOKED.getY());
+            spawnPacket.setPosZ(positionB.getZ() + OFFSET.HOOKED.getZ());
 
             DataWatcher meta = new DataWatcher();
             meta.set(EntityHandle.DATA_NO_GRAVITY, true);
@@ -158,26 +177,30 @@ public class VirtualFishingLine {
      * @param positionB End position of the line, a dobber is displayed here
      */
     public void updateViewers(Iterable<AttachmentViewer> viewers, Vector positionA, Vector positionB) {
-        // Teleport holder entity
-        if (this.holderEntityId != -1) {
-            final PacketPlayOutEntityTeleportHandle packet = PacketPlayOutEntityTeleportHandle.createNew(
-                    this.holderEntityId,
-                    positionA.getX() + OFFSET_HOLDER.getX(),
-                    positionA.getY() + OFFSET_HOLDER.getY(),
-                    positionA.getZ() + OFFSET_HOLDER.getZ(),
-                    0.0f, 0.0f, false);
-            viewers.forEach(v -> v.send(packet));
-        }
+        for (AttachmentViewer viewer : viewers) {
+            final Offsets OFFSET = offsets(viewer);
 
-        // Teleport hooked entity
-        {
-            final PacketPlayOutEntityTeleportHandle packet = PacketPlayOutEntityTeleportHandle.createNew(
-                    this.hookedEntityId,
-                    positionB.getX() + OFFSET_HOOKED.getX(),
-                    positionB.getY() + OFFSET_HOOKED.getY(),
-                    positionB.getZ() + OFFSET_HOOKED.getZ(),
-                    0.0f, 0.0f, false);
-            viewers.forEach(v -> v.send(packet));
+            // Teleport holder entity
+            if (this.holderEntityId != -1) {
+                final PacketPlayOutEntityTeleportHandle packet = PacketPlayOutEntityTeleportHandle.createNew(
+                        this.holderEntityId,
+                        positionA.getX() + OFFSET.HOLDER.getX(),
+                        positionA.getY() + OFFSET.HOLDER.getY(),
+                        positionA.getZ() + OFFSET.HOLDER.getZ(),
+                        0.0f, 0.0f, false);
+                viewer.send(packet);
+            }
+
+            // Teleport hooked entity
+            {
+                final PacketPlayOutEntityTeleportHandle packet = PacketPlayOutEntityTeleportHandle.createNew(
+                        this.hookedEntityId,
+                        positionB.getX() + OFFSET.HOOKED.getX(),
+                        positionB.getY() + OFFSET.HOOKED.getY(),
+                        positionB.getZ() + OFFSET.HOOKED.getZ(),
+                        0.0f, 0.0f, false);
+                viewer.send(packet);
+            }
         }
     }
 
@@ -237,5 +260,20 @@ public class VirtualFishingLine {
      */
     public void destroyLine(AttachmentViewer viewer) {
         viewer.send(PacketPlayOutEntityDestroyHandle.createNewSingle(this.hookEntityId));
+    }
+
+    private static class Offsets {
+        /** Offset to position the player so that the held fishing rod line is exactly at 0/0/0 */
+        public final Vector PLAYER;
+        /** Offset to position the silverfish mount of the player to align at 0/0/0 */
+        public final Vector HOLDER;
+        /** Offset to position the hooked silverfish entity to align hook-line at 0/0/0 */
+        public final Vector HOOKED;
+
+        public Offsets(Vector OFFSET_PLAYER, Vector OFFSET_HOLDER, Vector OFFSET_HOOKED) {
+            this.PLAYER = OFFSET_PLAYER;
+            this.HOLDER = OFFSET_HOLDER;
+            this.HOOKED = OFFSET_HOOKED;
+        }
     }
 }
