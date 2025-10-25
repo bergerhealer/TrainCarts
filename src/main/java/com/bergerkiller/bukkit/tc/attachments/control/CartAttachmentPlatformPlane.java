7329b8de8b0141a5507d8b22f01d528052c67f58
Irmo van den Berge <irmo.vandenberge@mailbox.org>
2025-10-26 00:09:30 +0200

    [Platform] Add a plane bbox particle for the plane mode platform
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@mailbox.org>

--- src/main/java/com/bergerkiller/bukkit/tc/attachments/control/CartAttachmentPlatformPlane.java
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/control/CartAttachmentPlatformPlane.java
@@ -1,10 +1,17 @@
 package com.bergerkiller.bukkit.tc.attachments.control;
 
 import com.bergerkiller.bukkit.common.config.ConfigurationNode;
+import com.bergerkiller.bukkit.common.math.Matrix4x4;
 import com.bergerkiller.bukkit.common.math.OrientedBoundingBox;
+import com.bergerkiller.bukkit.common.math.Quaternion;
 import com.bergerkiller.bukkit.common.math.Vector3;
+import com.bergerkiller.bukkit.common.utils.CommonUtil;
 import com.bergerkiller.bukkit.common.utils.LogicUtil;
-import org.bukkit.entity.Player;
+import com.bergerkiller.bukkit.tc.attachments.api.AttachmentManager;
+import com.bergerkiller.bukkit.tc.attachments.api.AttachmentViewer;
+import com.bergerkiller.bukkit.tc.attachments.helper.HelperMethods;
+import com.bergerkiller.bukkit.tc.attachments.particle.VirtualBoundingBox;
+import org.bukkit.ChatColor;
 import org.bukkit.util.Vector;
 
 /**
@@ -14,6 +21,7 @@ import org.bukkit.util.Vector;
  */
 public class CartAttachmentPlatformPlane extends CartAttachmentPlatform {
     private final OrientedBoundingBox bbox = new OrientedBoundingBox();
+    private Plane plane = null; // Null if not spawned
 
     @Override
     public void onLoad(ConfigurationNode config) {
@@ -36,14 +44,98 @@ public class CartAttachmentPlatformPlane extends CartAttachmentPlatform {
     }
 
     @Override
+    public void makeVisible(AttachmentViewer viewer) {
+        if (plane != null) {
+            plane.makeVisible(viewer);
+        }
+    }
+
+    @Override
+    public void makeHidden(AttachmentViewer viewer) {
+        if (plane != null) {
+            plane.makeHidden(viewer);
+        }
+    }
+
+    public void setPlaneColor(ChatColor color) {
+        if (color != null) {
+            if (plane == null) {
+                plane = new Plane(getManager(), bbox);
+                plane.entity.setGlowColor(color);
+                for (AttachmentViewer viewer : this.getAttachmentViewers()) {
+                    plane.makeVisible(viewer);
+                }
+            } else {
+                plane.entity.setGlowColor(color);
+            }
+        } else {
+            if (plane != null) {
+                plane.entity.setGlowColor(null);
+                plane.tickLastHidden = CommonUtil.getServerTicks();
+            }
+        }
+    }
+
+    @Override
+    public void onFocus() {
+        setPlaneColor(HelperMethods.getFocusGlowColor(this));
+    }
+
+    @Override
+    public void onBlur() {
+        setPlaneColor(null);
+    }
+
+    @Override
+    public void onTick() {
+        if (plane != null && !this.isFocused() &&
+                (CommonUtil.getServerTicks() - plane.tickLastHidden) > 40
+        ) {
+            plane.entity.destroyForAll();
+            plane = null;
+        }
+    }
+
+    @Override
+    public void onTransformChanged(Matrix4x4 transform) {
+        Quaternion orientation = transform.getRotation();
+        bbox.setPosition(transform.toVector().add(orientation.upVector().multiply(0.5 * bbox.getSize().getY())));
+        bbox.setOrientation(orientation);
+        if (plane != null) {
+            plane.update(bbox);
+        }
+    }
+
+    @Override
     public void onMove(boolean absolute) {
+        if (plane != null) {
+            plane.sync();
+        }
     }
 
-    @Override
-    public void makeVisible(Player viewer) {
-    }
+    private static class Plane {
+        public final VirtualBoundingBox entity;
+        private int tickLastHidden = 0;
 
-    @Override
-    public void makeHidden(Player viewer) {
+        public Plane(AttachmentManager manager, OrientedBoundingBox bbox) {
+            this.entity = VirtualBoundingBox.createPlane(manager);
+            this.entity.update(bbox);
+        }
+
+        public void update(OrientedBoundingBox bbox) {
+            this.entity.update(bbox);
+        }
+
+        public void sync() {
+            this.entity.syncPosition(true);
+        }
+
+        public void makeVisible(AttachmentViewer viewer) {
+            entity.spawn(viewer, new Vector(0.0, 0.0, 0.0));
+        }
+
+        public void makeHidden(AttachmentViewer viewer) {
+            entity.destroy(viewer);
+        }
     }
 }
