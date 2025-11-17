ef6c2d8ede5bfe935678c5490a2942a450d54eb3
Irmo van den Berge <irmo.vandenberge@mailbox.org>
2025-11-17 21:22:32 +0100

    Move all surface update logic to the surface tracker
    We cannot let attachments handle this because they do not know about the other surfaces
    
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@mailbox.org>

--- src/main/java/com/bergerkiller/bukkit/tc/attachments/surface/CollisionSurface.java
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/surface/CollisionSurface.java
@@ -1,12 +1,13 @@
 package com.bergerkiller.bukkit.tc.attachments.surface;
 
 import com.bergerkiller.bukkit.common.math.OrientedBoundingBox;
-import org.bukkit.block.BlockFace;
+import com.bergerkiller.bukkit.tc.attachments.api.AttachmentViewer;
 
 /**
- * A single collision surface. This surface can be cleared and then
- * surface elements can be added to it, without affecting other collision
- * surfaces that have been created.
+ * A single collision surface. When first created the surface is cleared.
+ * Call {@link #setShape(OrientedBoundingBox)} to set the position, orientation
+ * and size of the 2D surface. Then it becomes part of the collision surface
+ * simulation, and players will be able to walk on it.
  */
 public interface CollisionSurface {
     /** Surface used for disconnected players or when the plugin is disabled */
@@ -17,106 +18,43 @@ public interface CollisionSurface {
         }
 
         @Override
-        public void addSurface(OrientedBoundingBox surface) {
+        public void setShape(OrientedBoundingBox surface) {
         }
 
         @Override
-        public void addFloorTile(int x, int z, CollisionFloorTileShape shape) {
-        }
-
-        @Override
-        public void removeFloorTile(int x, int z) {
-        }
-
-        @Override
-        public void addWallTile(BlockFace face, int x, int y, double value) {
-        }
-
-        @Override
-        public void removeWallTile(BlockFace face, int x, int y) {
-        }
-
-        @Override
-        public void clear() {
+        public void remove() {
         }
     };
-    /** Default view range when the parameter is omitted */
-    int DEFAULT_VIEW_RANGE = 8;
+    /** Default distance from which shulkers spawn in when the parameter is omitted */
+    int DEFAULT_SHULKER_VIEW_DISTANCE = 8;
 
     /**
      * Gets the update counter state. This is incremented every time
-     * {@link #clear()} is called.
+     * {@link #remove()} or {@link #setShape(OrientedBoundingBox)} is called.
      *
      * @return Update counter state value
      */
     int getUpdateCounter();
 
     /**
-     * Adds a surface represented as an oriented bounding box. The surface
-     * is automatically transformed and quantized into suitable
+     * Sets the surface shape, represented as an oriented bounding box.
+     * The surface is automatically transformed and quantized into suitable
      * wall/ceiling/floor tiles. If the player is below a flat surface, regardless
-     * of orientation, it turns into ceiling tiles.
+     * of orientation, it turns into ceiling tiles.<br>
+     * <br>
+     * Call this every tick to make a moving surface. A moving surface is simulated
+     * separately using the {@link AttachmentViewer.MovementController}
      *
-     * @param surface Surface bounding box - the bottom face is used to represent
-     *                the surface.
+     * @param shape New surface shape
      */
-    void addSurface(OrientedBoundingBox surface);
+    void setShape(OrientedBoundingBox shape);
 
     /**
-     * Adds or updates a single shaped floor tile that is a part of this surface.
-     * Only a single tile can exist per x/z coordinate.
-     *
-     * @param x X-block coordinate
-     * @param z Z-block coordinate
-     * @param shape CollisionFloorTileShape
-     */
-    void addFloorTile(int x, int z, CollisionFloorTileShape shape);
-
-    /**
-     * Removes any shaped floor tile that was added at a particular x/z block
-     * coordinate previously. Can be called to undo a previous added shape.
-     * Practically not used, since you can just call clear() and add all shapes
-     * after, which is a lot more reliable.
-     *
-     * @param x X-block coordinate
-     * @param z Z-block coordinate
-     */
-    void removeFloorTile(int x, int z);
-
-    /**
-     * Adds a full-block wall tile, facing into a particular direction.
-     * The x/y are relative to the facing direction (horizontally y==y and x is the
-     * other axis, vertically the x==x and y is z).
-     *
-     * @param face Face direction of the player looking at the wall (opposite of normal)
-     * @param x Face-relative X tile
-     * @param y Face-relative Y tile
-     * @param value Axis value of the wall, depending on facing (the other axis)
-     */
-    void addWallTile(BlockFace face, int x, int y, double value);
-
-    /**
-     * Removes a previously added full-block wall tile, facing into a particular direction.
-     * The x/y are relative to the facing direction (horizontally y==y and x is the
-     * other axis, vertically the x==x and y is z).
-     *
-     * @param face Face direction of the player looking at the wall (opposite of normal)
-     * @param x Face-relative X tile
-     * @param y Face-relative Y tile
-     */
-    void removeWallTile(BlockFace face, int x, int y);
-
-    /**
-     * Clears this surface. All shapes that have been added so far will be removed
+     * Removes this surface. If any shape has been added before it will be removed
      * in the next tick, unless the shape is updated again right after clearing.<br>
      * <br>
-     * A typical update loop involves calling this clear() method and then
-     * calling methods like {@link #addFloorTile(int, int, CollisionFloorTileShape)}
-     * for all the tiles to render.<br>
-     * <br>
-     * This method also acts as the remove method. After calling clear, this
-     * surface instance can be safely discarded. It will automatically get
-     * cleaned up internally.
+     * It is allowed to call {@link #setShape(OrientedBoundingBox)} afterwards, which
+     * will re-add this surface.
      */
-    void clear();
+    void remove();
 }
